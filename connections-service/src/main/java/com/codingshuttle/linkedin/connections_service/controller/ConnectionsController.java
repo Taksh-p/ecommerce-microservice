e8617ccdda8188768a7f70cac8a2fc7597d6ac2f
Taksh-p <takshpatel2353@gmail.com>
2024-12-18 09:01:05 +0530

    Code added

---
+++ connections-service/src/main/java/com/codingshuttle/linkedin/connections_service/controller/ConnectionsController.java
@@ -0,0 +1,23 @@
+package com.codingshuttle.linkedin.connections_service.controller;
+
+import com.codingshuttle.linkedin.connections_service.entity.Person;
+import com.codingshuttle.linkedin.connections_service.service.ConnectionsService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/core")
+@RequiredArgsConstructor
+public class ConnectionsController {
+
+    private final ConnectionsService connectionsService;
+
+    @GetMapping("/first-degree")
+    public ResponseEntity<List<Person>> getFirstConnections() {
+        return ResponseEntity.ok(connectionsService.getFirstDegreeConnections());
+    }
+}
---
+++ posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/service/PostsService.java
@@ -0,0 +1,58 @@
+package com.codingshuttle.linkedin.posts_service.service;
+
+import com.codingshuttle.linkedin.posts_service.auth.UserContextHolder;
+import com.codingshuttle.linkedin.posts_service.clients.ConnectionsClient;
+import com.codingshuttle.linkedin.posts_service.dto.PersonDto;
+import com.codingshuttle.linkedin.posts_service.dto.PostCreateRequestDto;
+import com.codingshuttle.linkedin.posts_service.dto.PostDto;
+import com.codingshuttle.linkedin.posts_service.entity.Post;
+import com.codingshuttle.linkedin.posts_service.exception.ResourceNotFoundException;
+import com.codingshuttle.linkedin.posts_service.repository.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+    private final ModelMapper modelMapper;
+    private final ConnectionsClient connectionsClient;
+
+    public PostDto createPost(PostCreateRequestDto postDto, Long userId) {
+        Post post = modelMapper.map(postDto, Post.class);
+        post.setUserId(userId);
+
+        Post savedPost = postsRepository.save(post);
+        return modelMapper.map(savedPost, PostDto.class);
+    }
+
+    public PostDto getPostById(Long postId) {
+        log.debug("Retrieving post with ID: {}", postId);
+
+        Long userId = UserContextHolder.getCurrentUserId();
+
+        List<PersonDto> firstConnections = connectionsClient.getFirstConnections();
+
+//        TODO send Notifications to all connections
+
+        Post post = postsRepository.findById(postId).orElseThrow(() ->
+                new ResourceNotFoundException("Post not found with id: "+postId));
+        return modelMapper.map(post, PostDto.class);
+    }
+
+    public List<PostDto> getAllPostsOfUser(Long userId) {
+        List<Post> posts = postsRepository.findByUserId(userId);
+
+        return posts
+            .stream()
+            .map((element) -> modelMapper.map(element, PostDto.class))
+            .collect(Collectors.toList());
+    }
+}
